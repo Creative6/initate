d9e50a1542b1a04543ec7b0ce29739d98a58ffa7
FLOWER <1228745275@qq.com>
2020-10-19 10:48:42 +0800

    test

---
+++ build/precache-manifest.7dd3048b19895c2b80cf8277a192654b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9c4e8c59e2a4a73f83418549880de6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9860ee49ff2bc3b4f2d7",
+    "url": "./static/css/2.64c78a5b.chunk.css"
+  },
+  {
+    "revision": "645727a2d2146acdb1cd",
+    "url": "./static/css/main.ccd8cd34.chunk.css"
+  },
+  {
+    "revision": "9860ee49ff2bc3b4f2d7",
+    "url": "./static/js/2.d0139f28.chunk.js"
+  },
+  {
+    "revision": "4431613bff3a45544ca7136bb52528bc",
+    "url": "./static/js/2.d0139f28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "645727a2d2146acdb1cd",
+    "url": "./static/js/main.7db02c1b.chunk.js"
+  },
+  {
+    "revision": "5a6fe05e99d9b9cad124",
+    "url": "./static/js/runtime-main.74d55feb.js"
+  },
+  {
+    "revision": "63a50988eea349d1f07a9901330474f5",
+    "url": "./static/media/Mako-Regular-webfont.63a50988.ttf"
+  },
+  {
+    "revision": "ff6b6192a5213316b9f3f91a43b9e2d3",
+    "url": "./static/media/bg.ff6b6192.jpg"
+  },
+  {
+    "revision": "cb94ae751c5cfaf73775f9be2318dfba",
+    "url": "./static/media/icon.cb94ae75.gif"
+  },
+  {
+    "revision": "365b0ce03d3c23dfa37f5b1e0ef92b87",
+    "url": "./static/media/loading.365b0ce0.svg"
+  }
+]);
